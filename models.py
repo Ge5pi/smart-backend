5a7ae3f0969fafdd4bd8f01ca344f1c8849549ef
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-07-31 15:35:29 +0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=False)
     is_verified = Column(Boolean, default=False)
     files = relationship("File", back_populates="owner")
     connections = relationship("DatabaseConnection", back_populates="user")
