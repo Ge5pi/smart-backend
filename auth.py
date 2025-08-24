bc8433799ce71f53f17e79d63f40e034fc096c0a
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-08-24 16:01:53 +0500

    try

--- auth.py
+++ auth.py
@@ -74,6 +74,4 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 
 # Dependency для проверки, что пользователь не только аутентифицирован, но и активен
 def get_current_active_user(current_user: models.User = Depends(get_current_user)):
-    if not current_user.is_active:
-        raise HTTPException(status_code=403, detail="Inactive user or subscription expired")
     return current_user
